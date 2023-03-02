00130c893d390be67f85ec2aa4d5f35e3354c172
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-03-02 11:11:45 +0800

    Create ping.js

---
+++ src/commands/normal/ping.js
@@ -0,0 +1,10 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    aliases: ["pong"],
+    cooldown: 5000,//1 sec = 1000 ms / 0 = no cd.
+    run: async (client, message, args) => {
+      message.reply(`**\`\`\`asciidoc\nLatency :: ${Math.round(new Date() - message.createTimestamp)}ms \nWebsocket :: ${client.ping.ws}ms\n\`\`\`**`)
+    }
+ };
