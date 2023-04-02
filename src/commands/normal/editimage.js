866c6d6f5557287d07d6ef1c5fc5cb952a62a00e
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-04-02 17:53:17 +0800

    Add files via upload

---
+++ src/commands/normal/editimage.js
@@ -0,0 +1,14 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+module.exports = {
+    name: "editimage",
+    aliases: ["edimg", "editimg"],
+    cooldown: 5000,//1 sec = 1000 ms / 0 = no cd.
+    run: async (client, message, args) => {
+      
+      const em = new EmbedBuilder().setImage(args[1]).setColor(args[2] || "#B0FC38");
+
+      message.channel.send(args[0], { embeds: [em] })
+      
+    }
+ };
