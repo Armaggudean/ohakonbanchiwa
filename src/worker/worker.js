6231b32c397f16ca75915b068f2ab2409ed07b9c
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-07-23 11:33:24 +0800

    Update and rename worker.js to aternos.js

--- src/worker/worker.js
+++
@@ -1,6 +0,0 @@
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-const setting = require('../config.js');
-
-const chrome = require('selenium-webdriver/chrome');
-
-let driver = new Builder().forBrowser(Browser.CHROME).build();
