31cc35bfe2106c450f19679e9b41e0db96d80180
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-07-23 11:57:12 +0800

    Create _template.js

---
+++ src/commands/normal/_template.js
@@ -0,0 +1,10 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+module.exports = {
+    name: "",
+    aliases: [""],
+    cooldown: 5000,//1 sec = 1000 ms / 0 = no cd.
+    run: async (client, message, args) => {
+
+    }
+ };
