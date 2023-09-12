1241d8fb3a4f1c3cd400d461371f63a736b01a05
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-09-12 02:17:13 -0400

    Update avatar.js

--- src/commands/normal/avatar.js
+++ src/commands/normal/avatar.js
@@ -5,11 +5,8 @@ module.exports = {
     aliases: ["ava", "av"],
     cooldown: 5000,//1 sec = 1000 ms / 0 = no cd.
     run: async (client, message, args) => {
-        let mention = client.user.cache?.get(args[0]) || message.mentions.users.first() || message.author
-        let member = message.mentions.users.first() || message.guild.members.cache?.get(args[0]) || message.member;
-        const e = new EmbedBuilder()
-        .setColor(member.roles.cache.sort((a, b) => b.position - a.position).first().color)
-        .setImage(mention.displayAvatarURL({dynamic:true, size:256, extension:'png'}))
-        message.reply({content:`${message.author}`, embed: [e]})
+        const target = (await message.guild.resolveMember(args[0])) || message.member;
+        const response = avatarInfo(target.user);
+        await message.safeReply(response);
     },
 }
