9e51884af8725c791d16cb6a2156bcec4aaad999
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-09-12 02:31:19 -0400

    Update avatar.js

--- src/commands/normal/avatar.js
+++ src/commands/normal/avatar.js
@@ -6,7 +6,7 @@ module.exports = {
     cooldown: 5000,//1 sec = 1000 ms / 0 = no cd.
     run: async (client, message, args) => {
         const target = (await message.guild.resolveMember(args[0])) || message.member;
-        const response = avatarInfo(target.user);
-        await message.safeReply(response);
+        const response = target.user.displayAvatarURL();
+        await message.reply(response);
     },
 }
