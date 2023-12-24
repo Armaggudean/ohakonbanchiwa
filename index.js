a0563982d8462cb78ad7802ef0dafcea8fca7cea
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2023-12-24 19:02:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { prefix, owner } = require('./src/config');
 
-let TOKEN = process.env.token;
+let TOKEN = "NjY3NzU3MjI0MjM5ODI0OTAy.GMCpiS.BabBTETeVjpxyIJi60G5K5lDA0Luvkg88gH_to";
 
 client.commands = new Collection()
 client.slashcommands = new Collection()
