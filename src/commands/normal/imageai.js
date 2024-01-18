7f54922196528e70ec5501f950a3c185381de7aa
~Olivia <51457290+Armaggudean@users.noreply.github.com>
2024-01-18 00:29:15 -0300

    Update imageai.js

--- src/commands/normal/imageai.js
+++ src/commands/normal/imageai.js
@@ -1,20 +1,30 @@
-const { EmbedBuilder, PermissionsBitField } = require("discord.js");
-const { Hercai } = require('hercai');
 
-const herc = new Hercai();
+require('dotenv').config();
+const { default: axios } = require('axios');
+const { EmbedBuilder, PermissionsBitField, AttachmentBuilder } = require("discord.js");
 
 module.exports = {
     name: "imageai",
     aliases: ["drawimage"],
     cooldown: 5000,//1 sec = 1000 ms / 0 = no cd.
     run: async (client, message, args) => {
-        const salamcuy = args.join(' ');
-        
-        herc.drawImage({
-            model: 'v2',
-            prompt: salamcuy
-        }).then(r => {
-            message.reply(r.url)
-        })
+        const salamcuy = args.join('+');
+        const datalink = `https://skizo.tech/api/generatimg?prompt=${salamcuy}&apikey=${process.env.skikey}`;
+        message.channel.sendTyping();
+        try{
+            var axsos = await axios.get(datalink, {
+                responseType: 'arraybuffer'
+            })
+
+            const { data } = axsos;
+
+            message.reply({
+                content: 'nih resultnya mek',
+                files: [data]
+            })
+        } catch(e){
+            console.log(e)
+            message.reply('ada error ngabs')
+        }
     }
  };
